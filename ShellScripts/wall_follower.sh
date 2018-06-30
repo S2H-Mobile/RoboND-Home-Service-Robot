51746e6b2547b2f4a95cc92b0f4193b742cab30e
S2H-Mobile <S2H-Mobile@gmail.com>
2018-06-30 14:42:49 +0200

    fix: Improve mapping
    Modify world to start in corner. Modify gmapping parameters.

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roscore" & 
 sleep 3
-xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/World/u_shaped_room.world" & 
+xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/World/u_shaped_room3.world" & 
 sleep 14
-xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_navigation gmapping_demo.launch" & 
+xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_navigation gmapping_demo.launch custom_gmapping_launch_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/launch/turtlebot_kinect_gmapping.launch" & 
 sleep 6
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_rviz_launchers view_navigation.launch"& 
 sleep 6
