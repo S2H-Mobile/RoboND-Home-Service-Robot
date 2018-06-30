bca2613a36e1712fe846690032384ca0cc51f392
S2H-Mobile <S2H-Mobile@gmail.com>
2018-07-01 00:03:22 +0200

    fix: Rename world file

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roscore" & 
 sleep 3
-xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/World/u_shaped_room3.world" & 
+xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/World/u_shaped_room.world" & 
 sleep 14
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_navigation gmapping_demo.launch custom_gmapping_launch_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/launch/turtlebot_kinect_gmapping.launch" & 
 sleep 6
