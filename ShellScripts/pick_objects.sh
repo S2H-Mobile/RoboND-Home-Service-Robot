b880b5c7d878ca79599f993f4d429ba0479a263a
S2H-Mobile <S2H-Mobile@gmail.com>
2018-07-02 14:04:18 +0200

    feat: Customize visualization for pick_objects package

--- ShellScripts/pick_objects.sh
+++ ShellScripts/pick_objects.sh
@@ -5,6 +5,6 @@ xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/dev
 sleep 14
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/World/kinect-map.yaml" & 
 sleep 6
-xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; roslaunch pick_objects pick_objects_rviz.launch" & 
 sleep 6
 xterm  -e "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; rosrun pick_objects pick_objects"
