b766df3f12c5516c2bd0ea50668a8b60b0219ee7
S2H-Mobile <S2H-Mobile@gmail.com>
2018-07-03 00:38:36 +0200

    feat: Add home service functionality

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,109 +1,115 @@
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 #include <visualization_msgs/Marker.h>
 
-int main( int argc, char** argv )
+ros::Publisher marker_pub;
+visualization_msgs::Marker marker;
+
+const double pick_up_pose[3] = {6.5, 3.5, 0.0};
+const double drop_off_pose[3] = {4.0, -5.0, 0.0};
+
+void publish_marker()
 {
-    ros::init(argc, argv, "add_markers");
-    ros::NodeHandle n;
-    ros::Rate r(1);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
-    // Set the marker shape to a cube
-    uint32_t shape = visualization_msgs::Marker::CUBE;
-
-    // create the marker
-    visualization_msgs::Marker marker;
-
-    // Set the frame ID and timestamp
-    marker.header.frame_id = "/map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "basic_shapes";
-    marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
-
-    // Publish the marker at the pickup zone
-    marker.pose.position.x = 6.5;
-    marker.pose.position.y = 3.5;
-    marker.pose.position.z = 2.0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale
-    marker.scale.x = 0.45;
-    marker.scale.y = 0.45;
-    marker.scale.z = 0.45;
-
-    // Set the color
-    marker.color.r = 0.0f;
-    marker.color.g = 0.0f;
-    marker.color.b = 1.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
-
-    // Publish the marker at pick up zone
-    while (marker_pub.getNumSubscribers() < 1)
+  while (marker_pub.getNumSubscribers() < 1)
+  {
+    if (!ros::ok())
     {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
+      return;
     }
-    ROS_INFO("Publish marker at pick up zone.");
-    marker.action = visualization_msgs::Marker::ADD;
-    marker_pub.publish(marker);
+    ROS_WARN_ONCE("Please create a subscriber to the marker.");
+    sleep(1);
+  }
+  ROS_INFO_STREAM("Publishing marker at x=" << marker.pose.position.x << ", y=" << marker.pose.position.y);
+  marker_pub.publish(marker);
+}
 
+void odom_callback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+  // current pose of the robot from odometry
+  const float robot_x = msg -> pose.pose.position.x;
+  const float robot_y = msg -> pose.pose.position.y;
+
+  // Define a range delta
+  const double delta = 0.4;
+
+  // Robot is in zone if inside box of size delta x delta
+  const bool is_in_pickup_zone = abs(robot_x - pick_up_pose[0]) < delta && abs(robot_y - pick_up_pose[1]) < delta;
+  const bool is_in_dropoff_zone = abs(robot_x - drop_off_pose[0]) < delta && abs(robot_y - drop_off_pose[1]) < delta;
+
+  if (is_in_pickup_zone)
+  {
     // Pause 5 seconds
     ROS_INFO("Waiting 5 seconds ...");
     sleep(5);
 
     // Publish hiding marker
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
     ROS_INFO("Publish hiding marker.");
     marker.action = visualization_msgs::Marker::DELETE;
-    marker_pub.publish(marker);
+    publish_marker();
 
-    // Pause 5 seconds
-    ROS_INFO("Waiting 5 seconds ...");
-    sleep(5);
-
-    // Set marker data for drop off zone
-    marker.pose.position.x  = 4.0;
-    marker.pose.position.y = -5.0;
-    marker.pose.position.z = 4.0;
-
-    // Publish the marker at drop off zone
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-    ROS_INFO("Publish marker at drop off zone.");
+  } else if (is_in_dropoff_zone)
+  {
+    // Set marker data for drop off
+    marker.pose.position.x = drop_off_pose[0];
+    marker.pose.position.y = drop_off_pose[1];
+    marker.pose.position.z = drop_off_pose[2];
     marker.action = visualization_msgs::Marker::ADD;
-    marker_pub.publish(marker);
+    publish_marker();
+  }
+}
 
-    // Pause 5 seconds
-    ROS_INFO("Waiting 5 seconds ...");
-    sleep(5);
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
+    
+  // Create publisher for the marker 
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  // Create subscriber to the odom topic 
+  ros::Subscriber subscriber_odom = n.subscribe("odom", 1000, &odom_callback);
+
+  // Set marker data
+  marker.header.frame_id = "/map";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "basic_shapes";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.lifetime = ros::Duration();
+
+  // Set the orientation
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+
+  // Set the scale
+  marker.scale.x = 0.45;
+  marker.scale.y = 0.45;
+  marker.scale.z = 0.45;
+
+  // Set the color
+  marker.color.r = 0.0f;
+  marker.color.g = 0.0f;
+  marker.color.b = 1.0f;
+  marker.color.a = 1.0;
+
+  // Set pick up location
+  marker.pose.position.x = pick_up_pose[0];
+  marker.pose.position.y = pick_up_pose[1];
+  marker.pose.position.z = pick_up_pose[2];
+
+  // Publish the marker at pick up location
+  marker.action = visualization_msgs::Marker::ADD;
+  publish_marker();
+
+  // Listen to odom messages
+  ROS_INFO_STREAM("Start listening for odom messages.");
+  ros::Duration pause_duration(0.1);
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    pause_duration.sleep();
+  }
+  // ros::spin();
 }
